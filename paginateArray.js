53a63e192c0e58f586da234f533702194ed265c2
siwy256 <siwy256@gmail.com>
2022-01-01 18:36:16 +0100

    end?

---
+++ paginateArray.js
@@ -0,0 +1,30 @@
+import {paginateArrayValidation} from './paginanteArrayValidation.js';
+
+
+// turn off for tests:
+
+// /**
+//  * 
+//  * @param  {number[]} dataEntries array of entries - only numbers 
+//  * @param {{actualPageIdx: number, entriesOnPage: number}} settings index od page and number of entries on single page
+//  * @returns {number[]}
+//  */
+export const paginateArray = (dataEntries, settings) => {
+	paginateArrayValidation(dataEntries, settings);
+
+	const {actualPageIdx, entriesOnPage} = settings;	
+
+	/**
+	 * @type {number} - sample of usage type check 
+	 */
+  	const arrayLength = dataEntries.length;
+
+  	const pageCount = Math.floor(arrayLength / entriesOnPage);
+ 
+  	const firstEntry = entriesOnPage * actualPageIdx; 
+  	const lastEntry = firstEntry + entriesOnPage;
+
+	const entriesOnSelectedPage = dataEntries.slice(firstEntry, lastEntry);
+
+  	return entriesOnSelectedPage;  
+};
